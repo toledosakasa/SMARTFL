37427e73c987552f1304f90a76079dd9c6ade492
unknown <877292309@qq.com>
2020-04-02 01:16:31 +0800

    tests

---
+++ test/trace/BadReturnTest.java
@@ -0,0 +1,28 @@
+package trace;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BadReturnTest {
+
+	public int fact(int n) {
+		if(n <= 0)
+			return 1;
+		if(n == 2)
+			return 3;//wrong
+		int ret = fact(n-1);
+		return n*ret;
+	}
+
+	@Test
+	void pass() {
+		assertEquals(fact(0),0);
+	}
+	
+	@Test
+	void fail() {
+		assertEquals(fact(3),6);
+	}
+
+}
