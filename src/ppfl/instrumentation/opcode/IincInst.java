997f8a6e7d2d7a0820e9917f42345d70246438a3
unknown <1800010697@pku.edu.cn>
2020-06-28 11:14:57 +0800

    opcode87-147 except not implemented ones

---
+++ src/ppfl/instrumentation/opcode/IincInst.java
@@ -0,0 +1,36 @@
+package ppfl.instrumentation.opcode;
+
+import javassist.bytecode.BadBytecode;
+import javassist.bytecode.CodeIterator;
+import javassist.bytecode.ConstPool;
+import ppfl.instrumentation.CallBackIndex;
+import ppfl.instrumentation.opcode.OpcodeInst.paratype;
+
+//132
+public class IincInst extends OpcodeInst {
+
+	boolean isIntInst = true;
+	int loadedconst;
+
+	public IincInst(int _form) {
+		super(_form, 0, 0);
+	}
+
+	@Override
+	public String getinst(CodeIterator ci, int index, ConstPool constp) {
+		StringBuilder ret = new StringBuilder();
+		ret.append("opcode=" + this.opcode);
+		ret.append(",PARAVAR=" + this.para[0]);
+		ret.append(",PARACONST=" + this.para[1]);
+		return ret.toString();
+	}
+
+	@Override
+	public void insertByteCodeAfter(CodeIterator ci, int index, ConstPool constp, CallBackIndex cbi)
+			throws BadBytecode {
+		int instpos = ci.insertGap(4);// the gap must be long enough for the following instrumentation
+		ci.writeByte(184, instpos);// invokestatic
+		ci.write16bit(cbi.tsindex_int, instpos + 1);
+	}
+
+}
---
+++ src/ppfl/instrumentation/opcode/IlogicInst.java
@@ -0,0 +1,35 @@
+package ppfl.instrumentation.opcode;
+
+import javassist.bytecode.BadBytecode;
+import javassist.bytecode.CodeIterator;
+import javassist.bytecode.ConstPool;
+import ppfl.instrumentation.CallBackIndex;
+import ppfl.instrumentation.opcode.OpcodeInst.paratype;
+
+//126,128,130
+public class IlogicInst extends OpcodeInst {
+
+	boolean isIntInst = true;
+
+	public IlogicInst(int _form) {
+		super(_form, 1, 2);
+	}
+
+	@Override
+	public String getinst(CodeIterator ci, int index, ConstPool constp) {
+		StringBuilder ret = new StringBuilder();
+		ret.append("opcode=" + this.opcode);
+		ret.append(",popnum=" + this.popnum);
+		ret.append(",pushnum=" + this.pushnum);
+		return ret.toString();
+	}
+
+	@Override
+	public void insertByteCodeAfter(CodeIterator ci, int index, ConstPool constp, CallBackIndex cbi)
+			throws BadBytecode {
+		int instpos = ci.insertGap(4);// the gap must be long enough for the following instrumentation
+		ci.writeByte(184, instpos);// invokestatic
+		ci.write16bit(cbi.tsindex_int, instpos + 1);
+	}
+
+}
