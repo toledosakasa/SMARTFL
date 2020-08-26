8450306818fd4139328006b59f3110364a8e734f
unknown <877292309@qq.com>
2020-08-26 17:26:02 +0800

    test logger

--- src/ppfl/instrumentation/InstrumentationAgent.java
+++ src/ppfl/instrumentation/InstrumentationAgent.java
@@ -112,6 +112,7 @@ public class InstrumentationAgent {
 	private static void transform(Class<?> clazz, ClassLoader classLoader, Instrumentation instrumentation) {
 		TraceTransformer dt = new TraceTransformer(clazz.getName(), classLoader);
 		if (logFile != null) {
+			System.out.println(logFile);
 			dt.setLogFile(logFile);
 			dt.setSourceLogFile(logFile + clazz.getName() + ".source.log");
 		}
