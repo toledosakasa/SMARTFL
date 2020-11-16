e415cd02902a4b024835f0cb2588db738adce76e
toledosakasa <451712138@qq.com>
2020-11-17 01:43:32 +0800

    long setSize(2)

--- src/main/java/ppfl/instrumentation/opcode/LcmpInst.java
+++ src/main/java/ppfl/instrumentation/opcode/LcmpInst.java
@@ -1,8 +1,15 @@
 package ppfl.instrumentation.opcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javassist.bytecode.BadBytecode;
 import javassist.bytecode.CodeIterator;
 import javassist.bytecode.ConstPool;
+import ppfl.ByteCodeGraph;
+import ppfl.Node;
+import ppfl.ParseInfo;
+import ppfl.StmtNode;
 import ppfl.instrumentation.CallBackIndex;
 
 //148
@@ -24,6 +31,34 @@ public class LcmpInst extends OpcodeInst {
 		int instpos = ci.insertExGap(3);// the gap must be long enough for the following instrumentation
 		ci.writeByte(184, instpos);// invokestatic
 		ci.write16bit(cbi.tsindex_int, instpos + 1);
+	}
+
+	@Override
+	public void buildtrace(ByteCodeGraph graph) {
+		// build the stmtnode(common)
+		StmtNode stmt = buildstmt(graph);
+		ParseInfo info = graph.parseinfo;
+		List<Node> prednodes = new ArrayList<Node>();
+		List<Node> usenodes = new ArrayList<Node>();
+		Node defnode = null;
+		if (info.getintvalue("popnum") != null) {
+			int instpopnum = info.getintvalue("popnum");
+			for (int i = 0; i < instpopnum; i++) {
+				usenodes.add(graph.getRuntimeStack().pop());
+			}
+		}
+		if (info.getintvalue("pushnum") != null) {
+			int instpushnum = info.getintvalue("pushnum");
+			// push must not be more than 1
+			assert (instpushnum == 1);
+			defnode = graph.addNewStackNode(stmt);
+			defnode.setSize(2);
+		}
+		// build factor.
+		if (defnode != null) {
+			// TODO should consider ops.
+			graph.buildFactor(defnode, prednodes, usenodes, null, stmt);
+		}
 	}
 
 }
--- src/main/java/ppfl/instrumentation/opcode/LloadInst.java
+++ src/main/java/ppfl/instrumentation/opcode/LloadInst.java
@@ -1,8 +1,15 @@
 package ppfl.instrumentation.opcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javassist.bytecode.BadBytecode;
 import javassist.bytecode.CodeIterator;
 import javassist.bytecode.ConstPool;
+import ppfl.ByteCodeGraph;
+import ppfl.Node;
+import ppfl.ParseInfo;
+import ppfl.StmtNode;
 import ppfl.instrumentation.CallBackIndex;
 
 //22
@@ -27,6 +34,33 @@ public class LloadInst extends OpcodeInst {
 		int instpos = ci.insertExGap(3);// the gap must be long enough for the following instrumentation
 		ci.writeByte(184, instpos);// invokestatic
 		ci.write16bit(cbi.tsindex_long, instpos + 1);
+	}
+
+	@Override
+	public void buildtrace(ByteCodeGraph graph) {
+		// build the stmtnode(common)
+		StmtNode stmt = buildstmt(graph);
+		ParseInfo info = graph.parseinfo;
+		List<Node> prednodes = new ArrayList<Node>();
+		List<Node> usenodes = new ArrayList<Node>();
+		Node defnode = null;
+		if (info.getintvalue("load") != null) {
+			int loadvar = info.getintvalue("load");
+			Node node = graph.getLoadNodeAsUse(loadvar);
+			usenodes.add(node);
+		}
+		if (info.getintvalue("pushnum") != null) {
+			int instpushnum = info.getintvalue("pushnum");
+			// push must not be more than 1
+			assert (instpushnum == 1);
+			defnode = graph.addNewStackNode(stmt);
+			defnode.setSize(2);
+		}
+		// build factor.
+		if (defnode != null) {
+			// TODO should consider ops.
+			graph.buildFactor(defnode, prednodes, usenodes, null, stmt);
+		}
 	}
 
 }
