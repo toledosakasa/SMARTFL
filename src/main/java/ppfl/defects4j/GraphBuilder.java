0747b61f18034869ce5e76ec7c1544dccdd72922
unknown <877292309@qq.com>
2021-02-21 22:59:21 +0800

    clean

---
+++ src/main/java/ppfl/defects4j/GraphBuilder.java
@@ -0,0 +1,93 @@
+package ppfl.defects4j;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import ppfl.ByteCodeGraph;
+
+public class GraphBuilder {
+
+  private static void setupD4jProject(ByteCodeGraph pgraph, String project, int id) {
+    pgraph.useD4jTest = true;
+    String triggerTests = null;
+    String relevantClasses = null;
+    String allTestMethods = null;
+    String allTestClasses = null;
+    String configpath = String.format("d4j_resources/metadata_cached/%s%d.log", project, id);
+    try (BufferedReader reader = new BufferedReader(new FileReader(configpath));) {
+      String tmp;
+      while ((tmp = reader.readLine()) != null) {
+        String[] splt = tmp.split("=");
+        if (splt[0].equals("classes.relevant")) {
+          relevantClasses = splt[1];
+        }
+        if (splt[0].equals("tests.all")) {
+          allTestClasses = splt[1];
+        }
+        if (splt[0].equals("tests.trigger")) {
+          triggerTests = splt[1];
+        }
+        if (splt[0].equals("methods.test.all")) {
+          allTestMethods = splt[1];
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    if (relevantClasses != null) {
+      for (String s : relevantClasses.split(";")) {
+        if (!s.isEmpty()) {
+          pgraph.addTracedClass(s);
+          pgraph.parseD4jSource(project, id, s);
+        }
+      }
+    }
+    if (allTestClasses != null) {
+      for (String s : allTestClasses.split(";")) {
+        if (!s.isEmpty()) {
+          pgraph.addTracedClass(s);
+          pgraph.parseD4jSource(project, id, s);
+        }
+      }
+    }
+    if (triggerTests != null) {
+      for (String s : triggerTests.split(";")) {
+        if (!s.isEmpty())
+          pgraph.d4jTriggerTestNames.add(s);
+      }
+    }
+    if (allTestMethods != null) {
+      for (String s : allTestMethods.split(";")) {
+        if (!s.isEmpty())
+          pgraph.d4jMethodNames.add(s);
+      }
+    }
+    // long startTime = System.currentTimeMillis();
+    pgraph.get_idom();
+    pgraph.get_stores();
+    // long endTime = System.currentTimeMillis();
+    // long thetime = endTime-startTime;
+    // System.out.println("idom time is "+ thetime);
+    String tracefilename = String.format("tmp_checkout/%s%s/trace/logs/mytrace/all.log", project, id);
+    pgraph.parseJoinedTrace(tracefilename);
+    // this.parseD4jTrace(tracefilename);
+  }
+
+  public static void main(String args[]) {
+    ByteCodeGraph pgraph = new ByteCodeGraph();
+
+    String resultfile = "InfResult";
+    ByteCodeGraph.setResultLogger(resultfile);
+    String graphfile = "ProbGraph";
+    ByteCodeGraph.setGraphLogger(graphfile);
+
+    pgraph.setAutoOracle(true);
+    pgraph.setTraceAllClassed(false);
+    setupD4jProject(pgraph, "Lang", 3);
+    // pgraph.initD4jProject();
+    pgraph.printgraph();
+    pgraph.check_bp(true);
+  }
+}
