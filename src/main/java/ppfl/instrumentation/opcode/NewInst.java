79f9af15521abcc7ec27a9c8d7ece29f2b5eee97
Ultimanecat <877292309@qq.com>
2021-03-05 01:33:15 +0800

    fix newinst

--- src/main/java/ppfl/instrumentation/opcode/NewInst.java
+++ src/main/java/ppfl/instrumentation/opcode/NewInst.java
@@ -1,7 +1,10 @@
 package ppfl.instrumentation.opcode;
 
+import javassist.bytecode.BadBytecode;
 import javassist.bytecode.CodeIterator;
 import javassist.bytecode.ConstPool;
+import ppfl.ByteCodeGraph;
+import ppfl.instrumentation.CallBackIndex;
 
 //187
 public class NewInst extends OpcodeInst {
@@ -14,5 +17,18 @@ public class NewInst extends OpcodeInst {
 	public String getinst(CodeIterator ci, int index, ConstPool constp) {
 		StringBuilder ret = new StringBuilder(super.getinst(ci, index, constp));
 		return ret.toString();
+	}
+
+	@Override
+	public void insertByteCodeAfter(CodeIterator ci, int index, ConstPool constp, CallBackIndex cbi) throws BadBytecode {
+		int instpos = ci.insertExGap(3);// the gap must be long enough for the following instrumentation
+		ci.writeByte(184, instpos);// invokestatic
+		ci.write16bit(cbi.tsindex_object, instpos + 1);
+	}
+
+	@Override
+	public void buildtrace(ByteCodeGraph graph) {
+		super.buildtrace(graph);
+		defnode.setAddress(graph.parseinfo.getAddressFromStack());
 	}
 }
