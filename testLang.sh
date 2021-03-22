8aef345e6992de9a55eb60a18cd01dface759374
Ultimanecat <877292309@qq.com>
2021-03-22 20:04:12 +0800

    add script

--- testLang.sh
+++ testLang.sh
@@ -2,7 +2,7 @@
   
 for((i=1;i<=65;i++));  
 do   
-python3 s.py clearcache Lang $i
+echo running Lang $i
 timeout 1200 python3 s.py rerun Lang $i > trace/runtimelog/Lang$i.rund4j.log
 timeout 1200 python3 s.py parsed4j Lang $i > trace/runtimelog/Lang$i.parsed4j.log
 done  
