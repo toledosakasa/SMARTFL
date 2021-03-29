63d2648a3ab88d682e7033fdb97e7a6beee62319
Ultimanecat <877292309@qq.com>
2021-03-30 01:02:25 +0800

    skip 43 to avoid memory problem

--- testLang.sh
+++ testLang.sh
@@ -1,8 +1,15 @@
 #!/bin/bash  
   
-for((i=1;i<=65;i++));  
+for((i=1;i<=42;i++));  
 do   
 echo running Lang $i
-timeout 1200 python3 s.py rerun Lang $i > trace/runtimelog/Lang$i.rund4j.log
-timeout 1200 python3 s.py parsed4j Lang $i > trace/runtimelog/Lang$i.parsed4j.log
+timeout 600 python3 s.py rerun Lang $i > trace/runtimelog/Lang$i.rund4j.log
+timeout 600 python3 s.py parsed4j Lang $i > trace/runtimelog/Lang$i.parsed4j.log
+done  
+
+for((i=44;i<=65;i++));  
+do   
+echo running Lang $i
+timeout 600 python3 s.py rerun Lang $i > trace/runtimelog/Lang$i.rund4j.log
+timeout 600 python3 s.py parsed4j Lang $i > trace/runtimelog/Lang$i.parsed4j.log
 done  
