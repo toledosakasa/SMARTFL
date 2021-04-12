2c3aec5bfcd6a636e36fb506fb3083cc328c218e
Ultimanecat <877292309@qq.com>
2021-04-12 19:24:57 +0800

    fix a keyError crash when nested test-calls occur

--- pylib/countmap.py
+++ pylib/countmap.py
@@ -23,17 +23,17 @@ class CountMap:
         max_class_count = 5
         sorted_classes = sorted(self.class_relevant_cnt.keys(
         ), key=lambda t: self.class_relevant_cnt[t]/self.class_method_cnt[t], reverse=True)
-        sorted_classes = sorted_classes[:max_class_count]
+        sorted_classes_trim = sorted_classes[:max_class_count]
 
         # always keep trigger test classes.
         for triggerclass in triggertests:
-            if not triggerclass in sorted_classes:
-                sorted_classes.append(triggerclass)
+            if (not triggerclass in sorted_classes_trim) and (triggerclass in sorted_classes):
+                sorted_classes_trim.append(triggerclass)
 
         # get methods
         max_method_count = 5
         ret = []
-        for curclass in sorted_classes:
+        for curclass in sorted_classes_trim:
             curmap = self.relevant_cnt[curclass]
             sorted_methods = sorted(
                 curmap.keys(), key=lambda t: curmap[t], reverse=True)
